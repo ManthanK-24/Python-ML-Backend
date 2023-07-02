88c025f9b0358a88cbd8bee4507f1032998d0f7d
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2023-07-02 19:48:30 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -10,3 +10,24 @@ print(String1[3:12])
 # 3rd and 2nd last character
 print("\nSlicing characters between " + "3rd and 2nd last character: ")
 print(String1[3:-2])
+
+# Python Program to Update
+# character of a String
+
+String1 = "Hello, I'm a Geek"
+print("Initial String: ")
+print(String1)
+
+# Updating a character of the String
+## As Python strings are immutable, they don't support item updation directly
+### There are following two ways
+#1
+list1 = list(String1)
+list1[2] = 'p'
+String2 = ''.join(list1)
+print("\nUpdating character at 2nd Index: ")
+print(String2)
+
+#2
+String3 = String1[0:2] + 'p' + String1[3:]
+print(String3)

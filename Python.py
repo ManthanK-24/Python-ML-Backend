17f841b6b4ff5fdc9d91800ab44d7b558953dd50
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2023-07-02 19:46:11 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,12 @@
+# Tuple is like a list but we can't modify, tuple once declared
+tup = (1,2,3)
+li = [1,2,3]
+
+# Printing 3rd to 12th character
+print("\nSlicing characters from 3-12: ")
+print(String1[3:12])
+ 
+# Printing characters between
+# 3rd and 2nd last character
+print("\nSlicing characters between " + "3rd and 2nd last character: ")
+print(String1[3:-2])
